62fb3e06d47808c13e29901d2a3c35b025efc22f
Ömer Tufan Ayhan <omertufan2000@hotmail.com>
2022-11-02 00:27:54 +0300

    basic of mediatr done

---
+++ MediatorPattern/MediatrWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediatrWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CalculatorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("Multiply")]
+        public async Task<int> Multiply(int val1, int val2)
+        {
+            MultiplyRequest request = new MultiplyRequest() { Val1 = val1, Val2 = val2 };
+            return await _mediator.Send(request);
+        }
+    }
+}
