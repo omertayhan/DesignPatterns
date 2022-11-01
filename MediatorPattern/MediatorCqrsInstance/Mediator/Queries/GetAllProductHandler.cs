8686478890a0f3560a9322f6c27905f7d98e195e
Ömer Tufan Ayhan <omertufan2000@hotmail.com>
2022-11-02 01:29:28 +0300

    cqrs and mediator done

---
+++ MediatorPattern/MediatorCqrsInstance/Mediator/Queries/GetAllProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatorExample.Mediator.Queries;
+using MediatR;
+
+namespace MediatrCqrsInstance.Mediator.Queries
+{
+    public class GetAllProductHandler : IRequestHandler<GetAllProductQuery, List<GetProductViewModel>>
+    {
+        public Task<List<GetProductViewModel>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+        {
+            var model = new GetProductViewModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Book"
+            };
+            var model2 = new GetProductViewModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Monitor"
+            };
+            return Task.FromResult(new List<GetProductViewModel>() { model, model2 });
+        }
+    }
+}
