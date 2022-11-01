19cc33e99df1edf25f3afca87b695b6218029a0b
Ömer Tufan Ayhan <omertufan2000@hotmail.com>
2022-11-02 01:55:13 +0300

    .

--- MediatorPattern/MediatrCqrsWebApi/Program.cs
+++ MediatorPattern/MediatrCqrsWebApi/Program.cs
@@ -1,16 +1,13 @@
-using System.Reflection;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddMediatR(typeof(Program));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
