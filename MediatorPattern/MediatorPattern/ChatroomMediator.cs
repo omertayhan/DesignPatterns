6c46d56be172178a0ec882ed5d464bf69ad0e5c2
Ömer Tufan Ayhan <omertufan2000@hotmail.com>
2022-11-01 16:02:58 +0300

    mediator basics

---
+++ MediatorPattern/MediatorPattern/ChatroomMediator.cs
@@ -0,0 +1,27 @@
+﻿namespace MediatorPattern
+{
+    public class ChatroomMediator : IChatroomMediator
+    {
+        private Dictionary<string, User> _user = new Dictionary<string, User>();
+
+        public void Register(User user)
+        {
+            if (!_user.ContainsValue(user))
+            {
+                _user[user.Name] = user;
+            }
+
+            user.chatRoomMediator=this;
+        }
+
+        public void Send(string from, string to, string message)
+        {
+            User user = _user[to];
+
+            if (user !=null)
+            {
+                user.Receive(from,message);
+            }
+        }
+    }
+}
---
+++ MediatorPattern/MediatorPattern/IChatroomMediator.cs
@@ -0,0 +1,8 @@
+﻿namespace MediatorPattern
+{
+    public interface IChatroomMediator
+    {
+        public void Register(User user);
+        public void Send(string from, string to, string message);
+    }
+}
